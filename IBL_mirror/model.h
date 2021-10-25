d0f76d3b74e130e1e6f782c12bcd05fd449f808e
VaNRoZ <giga1345@gmail.com>
2021-10-25 21:11:00 +0300

    Try16

--- IBL_mirror/model.h
+++ IBL_mirror/model.h
@@ -26,19 +26,19 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
 class Model
 {
 public:
-	// Данные модели 
-	vector<Texture> textures_loaded; // (оптимизация) сохраняем все загруженные текстуры, чтобы убедиться, что они не загружены более одного раза
+	// model information
+	vector<Texture> textures_loaded; 
 	vector<Mesh> meshes;
 	string directory;
 	bool gammaCorrection;
 
-	// Конструктор в качестве аргумента использует путь к 3D-модели
+	// path to the 3D model
 	Model(string const &path, bool gamma = false) : gammaCorrection(gamma)
 	{
 		loadModel(path);
 	}
 
-	// Отрисовываем модель, а значит и все её меши
+	// Drawing the model
 	void Draw(Shader shader)
 	{
 		for (unsigned int i = 0; i < meshes.size(); i++)
@@ -46,40 +46,37 @@ public:
 	}
 
 private:
-	// Загружаем модель с помощью Assimp и сохраняем полученные меши в векторе meshes
+	// loading model with Assimp and save in meshes
 	void loadModel(string const &path)
 	{
-		// Чтение файла с помощью Assimp
+		// reading file in Assimp
 		Assimp::Importer importer;
 		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
 
-		// Проверка на ошибки
-		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // если НЕ 0
+		// error chking
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) 
 		{
 			cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
 			return;
 		}
 
-		// Получение пути к файлу
+		// file path
 		directory = path.substr(0, path.find_last_of('/'));
 
-		// Рекурсивная обработка корневого узла Assimp
+		// Recursive processing of the root node Assimp
 		processNode(scene->mRootNode, scene);
 	}
 
-	// Рекурсивная обработка узла. Обрабатываем каждый отдельный меш, расположенный в узле, и повторяем этот процесс для дочерних узлов (если таковые вообще имеются)
+	// Recursive processing
 	void processNode(aiNode *node, const aiScene *scene)
 	{
-		// Обрабатываем каждый меш текущего узла
+		// processing mesh
 		for (unsigned int i = 0; i < node->mNumMeshes; i++)
 		{
-			// Узел содержит только индексы объектов в сцене.
-			// Сцена же содержит все данные; узел - это лишь способ организации данных
 			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
 			meshes.push_back(processMesh(mesh, scene));
 		}
 
-		// После того, как мы обработали все меши (если таковые имелись), мы начинаем рекурсивно обрабатывать каждый из дочерних узлов
 		for (unsigned int i = 0; i < node->mNumChildren; i++)
 		{
 			processNode(node->mChildren[i], scene);
@@ -89,36 +86,34 @@ private:
 
 	Mesh processMesh(aiMesh *mesh, const aiScene *scene)
 	{
-		// Данные для заполнения
+		// Information to fill out
 		vector<Vertex> vertices;
 		vector<unsigned int> indices;
 		vector<Texture> textures;
 
-		// Цикл по всем вершинам меша
+		//Cycle through all meshes vertices
 		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
 		{
 			Vertex vertex;
-			glm::vec3 vector; // мы объявляем промежуточный вектор, т.к. assimp использует свой собственный векторный класс, который не преобразуется напрямую в тип glm::vec3, поэтому сначала мы передаем данные в этот промежуточный вектор типа glm::vec3
+			glm::vec3 vector; 
 
-			// Координаты
+			// coordinates
 			vector.x = mesh->mVertices[i].x;
 			vector.y = mesh->mVertices[i].y;
 			vector.z = mesh->mVertices[i].z;
 			vertex.Position = vector;
 
-			// Нормали
+			// normal
 			vector.x = mesh->mNormals[i].x;
 			vector.y = mesh->mNormals[i].y;
 			vector.z = mesh->mNormals[i].z;
 			vertex.Normal = vector;
 
-			// Текстурные координаты
-			if (mesh->mTextureCoords[0]) // если меш содержит текстурные координаты
+			// texturs coordinate
+			if (mesh->mTextureCoords[0]) 
 			{
 				glm::vec2 vec;
 
-				// Вершина может содержать до 8 различных текстурных координат. Мы предполагаем, что не будем использовать модели,
-				// в которых вершина может содержать несколько текстурных координат, поэтому мы всегда берем первый набор (0)
 				vec.x = mesh->mTextureCoords[0][i].x;
 				vec.y = mesh->mTextureCoords[0][i].y;
 				vertex.TexCoords = vec;
@@ -126,13 +121,13 @@ private:
 			else
 				vertex.TexCoords = glm::vec2(0.0f, 0.0f);
 
-			// Касательный вектор
+			// Tangent vector
 			vector.x = mesh->mTangents[i].x;
 			vector.y = mesh->mTangents[i].y;
 			vector.z = mesh->mTangents[i].z;
 			vertex.Tangent = vector;
 
-			// Вектор бинормали
+			// Binormal Vector
 			vector.x = mesh->mBitangents[i].x;
 			vector.y = mesh->mBitangents[i].y;
 			vector.z = mesh->mBitangents[i].z;
@@ -140,48 +135,39 @@ private:
 			vertices.push_back(vertex);
 		}
 
-		// Теперь проходимся по каждой грани меша (грань - это треугольник меша) и извлекаем соответствующие индексы вершин
 		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
 		{
 			aiFace face = mesh->mFaces[i];
 
-			// Получаем все индексы граней и сохраняем их в векторе indices
+			// edge index in vectore indices
 			for (unsigned int j = 0; j < face.mNumIndices; j++)
 				indices.push_back(face.mIndices[j]);
 		}
 
-		// Обрабатываем материалы
+		// Processing materials
 		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
 
-		// Мы вводим соглашение об именах сэмплеров в шейдерах. Каждая диффузная текстура будет называться 'texture_diffuseN',
-		// где N - это порядковый номер от 1 до MAX_SAMPLER_NUMBER. 
-		// То же самое относится и к другим текстурам:
-		// диффузная: texture_diffuseN
-		// отражения: texture_specularN
-		// нормали: texture_normalN
 
-		// 1. Диффузные карты
+		// 1. Diffuse maps
 		vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
 		textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
 
-		// 2. Карты отражения
+		// 2. Reflection Maps
 		vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
 		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
 
-		// 3. Карты нормалей
+		// 3. Normal maps
 		std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
 		textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
 
-		// 4. Карты высот
+		// 4. Elevation maps
 		std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
 		textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
 
-		// Возвращаем меш-объект, созданный на основе полученных данных
+		// Returning a mash object
 		return Mesh(vertices, indices, textures);
 	}
 
-	// Проверяем все текстуры материалов заданного типа и загружам текстуры, если они еще не были загружены.
-	// Необходимая информация возвращается в виде структуры Texture
 	vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName)
 	{
 		vector<Texture> textures;
@@ -190,25 +176,25 @@ private:
 			aiString str;
 			mat->GetTexture(type, i, &str);
 
-			// Проверяем, не была ли текстура загружена ранее, и если - да, то пропускаем загрузку новой текстуры и переходим к следующей итерации
+			// cheking to double uploading texture
 			bool skip = false;
 			for (unsigned int j = 0; j < textures_loaded.size(); j++)
 			{
 				if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
 				{
 					textures.push_back(textures_loaded[j]);
-					skip = true; // текстура с тем же путем к файлу уже загружена, переходим к следующей (оптимизация)
+					skip = true; 
 					break;
 				}
 			}
 			if (!skip)
-			{   // если текстура еще не была загружена, то загружаем её
+			{   
 				Texture texture;
 				texture.id = TextureFromFile(str.C_Str(), this->directory);
 				texture.type = typeName;
 				texture.path = str.C_Str();
 				textures.push_back(texture);
-				textures_loaded.push_back(texture); // сохраняем текстуру в массиве с уже загруженными текстурами, тем самым гарантируя, что у нас не появятся без необходимости дубликаты текстур
+				textures_loaded.push_back(texture); 
 			}
 		}
 		return textures;
